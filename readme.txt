c3e03cc1e8ba7398e63d3c59c627faaaecd80857
k2comma <k2comma@dongbuinc.com>
2017-06-26 13:02:45 +0900

    initialize...

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Link URL
+1. Original source:
+    http://millky.com/@origoni/post/1100
+
+2. about Lombok
+    http://edoli.tistory.com/99
+
+
---
+++ src/main/java/xyz/devmaru/exam/spring/boot/blog/Blog4eduApplication.java
@@ -0,0 +1,12 @@
+package xyz.devmaru.exam.spring.boot.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog4eduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog4eduApplication.class, args);
+	}
+}
